04b9d64ad5cd201157b7c99293a2341bdc6fd488
timilehinjegede <timilehintimothy.jegede@gmail.com>
2020-04-28 17:57:07 +0100

    Added app colors

---
+++ fruit_store/lib/utils/colors.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class AppColors{
+
+  static final bkColor = Color(0XFFFFD818);
+  static final firstCardColor = Color(0XFFF5E0BA);
+  static final secondCardColor = Color(0XFFFFC599);
+  static final thirdCardColor = Color(0XFFDAE69C);
+  static final iconColors = Color(0XFFA87254);
+    
+}
