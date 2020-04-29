48dfe220ea3038695865ee96cfe67a6295855a19
timilehinjegede <timilehintimothy.jegede@gmail.com>
2020-04-29 21:01:44 +0100

    Added images to the project

---
+++ wine_store/lib/utils/images.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class AppImages{
+
+  static final AssetImage wine0 = AssetImage('assets/images/wine0.png') ;
+
+  static final AssetImage wine1 = AssetImage('assets/images/wine1.png') ;
+
+  static final AssetImage wine2 = AssetImage('assets/images/wine2.png') ;
+
+  static final AssetImage wine3 = AssetImage('assets/images/wine3.png') ;
+
+  static final AssetImage wine4 = AssetImage('assets/images/wine4.png') ;
+
+
+}
