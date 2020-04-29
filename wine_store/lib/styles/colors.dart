401660beb23519e025e8badab1db88fcae3859e4
timilehinjegede <timilehintimothy.jegede@gmail.com>
2020-04-29 21:05:29 +0100

    Added app colors to the project

---
+++ wine_store/lib/styles/colors.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class AppColors{
+
+  static final kWhite = Colors.white;
+  static final kBlack = Colors.black;
+  static final kGrey = Colors.grey;
+
+  static final kOrange = Color.fromRGBO(245, 152, 52, 1);
+  static final kPink = Color.fromRGBO(253, 86, 92, 1);
+}
